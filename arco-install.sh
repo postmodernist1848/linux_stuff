8c12bc2d77ed0eb8a7dfceb81c3ae0462a8842a9
postmodernist1488 <elitic.pantheism@gmail.com>
2023-05-06 20:04:14 +0300

    update

--- arco-install.sh
+++ arco-install.sh
@@ -79,7 +79,7 @@ cp /run/media/postmodernist1488/mountpoint/wallpapers/ -r $HOME/Pictures/
 echo "Copying .ssh from $partition..."
 cp /run/media/postmodernist1488/mountpoint/.ssh/ -r $HOME/
 chmod 600 $HOME/.ssh/{id_rsa,known_hosts}
-chmod 644 $HOME/.ssh/{id_rds.pub,githubpat}
+chmod 644 $HOME/.ssh/{id_rsa.pub,githubpat}
 
 sudo umount -v $partition
 sudo rmdir /run/media/postmodernist1488/mountpoint
