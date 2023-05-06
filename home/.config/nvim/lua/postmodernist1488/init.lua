9d9a62dcaaf5966675f9ae424147139eafb1ee1b
postmodernist1488 <elitic.pantheism@gmail.com>
2023-05-06 17:59:24 +0300

    allow for packer bootstrapping

--- home/.config/nvim/lua/postmodernist1488/init.lua
+++ home/.config/nvim/lua/postmodernist1488/init.lua
@@ -1,5 +1,5 @@
+require('postmodernist1488.packer')
 require('postmodernist1488.set')
 require('postmodernist1488.colors')
-require('postmodernist1488.packer')
 require('postmodernist1488.terminal')
 require('postmodernist1488.remap')
